33be158f37953826a3998dc6b8b1ff84226a5377
Osman Sarı <osman.sari066@gmail.com>
2020-09-09 01:55:50 +0300

    Popup component
    Searching with name, date and type is done. User can see details of movie bye Popup.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,124 @@
 import React, {useState} from 'react';
 import Search from './components/Search'
 import Results from './components/Results'
+import Popup from './components/Popup'
+
 
 import axios from 'axios'
 
 function App() {
   const [state, setState] = useState ({
+  
     s:"",
+    y:"",
+    t:"",
+    
     results:[],
     selected: {} 
 
   });
   const apiurl = "http://www.omdbapi.com/?apikey=a73c582f";
+
   
-  const search = (e) => {
-    if(e.key === "Enter"){
-      axios(apiurl + "&s=" + state.s).then(({data}) =>{
-       let results = data.Search;
+//Default Pokemon search
+// if(state.s ==="" && state.y === "" && state.t === ""){
+//   axios(apiurl + "&s=pokemon" ).then(({data}) =>{
+//     let results = data.Search;
 
-       setState(prevState =>{
-         return { ...prevState, results:results }
-       })
-      })
-    }
-  }
+//     setState(prevState =>{
+//       return { ...prevState, results:results }
+//     })
+//    })
  
-  const handleInput = (e) => {
-    let s = e.target.value;
+  
+// }
+// else{
+//   console.log("searching..") 
+// }
 
+const search = (e) => {
+  state.results=[];
+ 
+  if(e.key === "Enter"){
+    
+    axios(apiurl + "&s=" + state.s).then(({data}) =>{
+     let results = data.Search;
+     setState(prevState =>{
+       return { ...prevState, results:results }
+     })
+    })
+  }
+}
+
+const searchyear = (e) => {
+  if(e.key === "Enter"){
+    
+    axios(apiurl + "&s=" + state.s+"&y="+state.y).then(({data}) =>{
+     let results = data.Search;
+     setState(prevState =>{
+       return { ...prevState, results:results }
+     })
+    })
+  }
+}
+
+const searchtype = (e) => {
+  if(e.key === "Enter"){
+    
+    axios(apiurl + "&s=" + state.s+"&type="+state.t).then(({data}) =>{
+     let results = data.Search;
+     setState(prevState =>{
+       return { ...prevState, results:results }
+     })
+    })
+  }
+}
+
+
+
+  const handleInput = (e) => {  
+    let s = e.target.value;
     setState(prevState=>{
       return{ ...prevState, s:s}
 
     })
-
-   
-    
   }
 
-  
+  const handleInputYear = (e) => {  
+   
+    let y = e.target.value;
+    setState(prevState=>{
+      return{ ...prevState, y:y}
+
+    })
+  }
+
+  const handleInputType = (e) => {  
+   
+    let t = e.target.value;
+    setState(prevState=>{
+      return{ ...prevState, t:t}
+
+    })
+  }
+
+  const openPopup = id => {
+    axios (apiurl + "&i=" + id).then(({data}) =>{
+      let result = data;
+
+      setState(prevState => {
+        return { ...prevState, selected:result}
+      });
+ 
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+
 
   return (
     <div className="App">
@@ -45,9 +126,13 @@ function App() {
        <h1>Movie List Project</h1>
       </header>
       <main>
-        <Search handleInput={ handleInput} search={ search} />
-        <Results results={state.results} />
-      </main>
+
+        <Search handleInput={ handleInput} handleInputYear={ handleInputYear} handleInputType={ handleInputType}
+         search={ search} searchyear={ searchyear}  searchtype={ searchtype} />
+        
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false} </main>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,32 @@
 import React from 'react'
 
-function Search( {handleInput, search} ) {
+function Search( {handleInput,handleInputYear,handleInputType, search, searchyear, searchtype } ) {
     return (
       <section className="searchBox-section">
 
           <input 
           type="text" 
-          placeholder="Search with movie name" 
+          placeholder="Search with name (required)" 
           className="searchbox"
           onChange={handleInput}
           onKeyPress={search} 
           />
-
+<br></br>
+         <input 
+          type="text" 
+          placeholder="Search with date" 
+          className="searchbox"
+          onChange={handleInputYear}
+          onKeyPress={searchyear} 
+          />
+          <br></br>
+         <input 
+          type="text" 
+          placeholder="Search with type" 
+          className="searchbox"
+          onChange={handleInputType}
+          onKeyPress={searchtype} 
+          />
           
 
 
